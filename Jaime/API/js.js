45e62f052080749a5e21f94c6a8575abe7abe725
jmenadesign <45119807+jmenadesign@users.noreply.github.com>
2019-01-11 09:10:28 +0100

    Update js.js

--- Jaime/API/js.js
+++ Jaime/API/js.js
@@ -1,25 +1,3 @@
-/*
-const apiUrl = 'https://swapi.co/api/planets'
-
-
-async function llamadaApi(){
-    let planets = await fetch(apiUrl)
-    planets = await planets.json()
-    console.log(planets.results) // 0 a 9 name y population
-    representar(planets.results)
-}
-
-llamadaApi()
-
-const pintame = document.getElementById('pintame')
-
-function representar(planetList){
-    planetList.forEach(planet => { // function(planet) se puede sustituir por planet =>
-        pintame.innerHTML += `<b>${planet.name}</b> population: ${planet.population} <br>`
-    });
-}
-*/
-
 const apiUrl = 'http://api.icndb.com/jokes/random/5'
 
 
